325ab3ab78766cdb8b6c3c8995f63ec8bdb782dd
Aleks Kamko <aykamko@gmail.com>
2016-07-09 12:07:36 -0700

    geonotes routes stuff

---
+++ rails_api/app/controllers/geonotes_controller.rb
@@ -0,0 +1,14 @@
+class GeoNotesController < ApplicationController
+  def show
+    GeoNote.find(params.require(:id))
+  end
+
+  def create
+    GeoNote.new(params.require(:latitude, :longitude, :phone_id, :note_text))
+  end
+
+  def in_bounds
+    params = params.require(sw: [:latitude, :longitude], ne: [:latitude, :longitude])
+    GeoNote.in_bounds([params.sw, params.ne]).all
+  end
+end
