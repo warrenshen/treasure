b73c0a0fb131673e12ea9282fa2742e2b9c918eb
warrenshen <warrenzshen@gmail.com>
2016-07-09 11:59:52 -0700

    Initialize clean react-native project

--- ios/Pods/Target Support Files/Pods-Treasure/Pods-Treasure-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Treasure : NSObject
-@end
-@implementation PodsDummy_Pods_Treasure
-@end
--- ios/Pods/Target Support Files/Pods-TreasureTests/Pods-TreasureTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TreasureTests : NSObject
-@end
-@implementation PodsDummy_Pods_TreasureTests
-@end
