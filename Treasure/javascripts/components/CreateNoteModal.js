703e0bc18673a6738f9be62ff8afd1e398db7cda
Daniel Li <danielmxli@gmail.com>
2016-07-09 14:51:54 -0700

    Add TextInput box

--- Treasure/javascripts/components/CreateNoteModal.js
+++ Treasure/javascripts/components/CreateNoteModal.js
@@ -3,23 +3,26 @@ import React, { Component } from 'react';
 
 // UI
 import {
+  AppRegistry,
   StyleSheet,
   Text,
   View,
   TouchableHighlight,
   Modal,
+  TextInput,
 } from 'react-native';
 
 class CreateNoteModal extends Component {
 
   // --------------------------------------------------
-  // Props
-  // --------------------------------------------------
-
-
-  // --------------------------------------------------
   // State
   // --------------------------------------------------
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "",
+    };
+  }
 
   // --------------------------------------------------
   // Render
@@ -30,21 +33,37 @@ class CreateNoteModal extends Component {
         animationType={"slide"}
         transparent={false}
         visible={this.props.isVisible}
-        onRequestClose={() => {alert("Modal has been closed.")}}
-        >
-       <View style={{marginTop: 22}}>
-        <View style={styles.actions}>
-          <TouchableHighlight
-            onPress={this.props.onPost}
-            style={styles.button}>
-            <Text>Post</Text>
-          </TouchableHighlight>
-          <TouchableHighlight
-            onPress={this.props.onCancel}
-            style={styles.button}>
-            <Text>Cancel</Text>
-          </TouchableHighlight>
+        onRequestClose={() => {alert("Modal has been closed.")}}>
+        <View style={styles.header}>
+          <View style={styles.action}>
+            <TouchableHighlight
+              onPress={this.props.onCancel}
+              style={styles.button}>
+              <Text>Cancel</Text>
+            </TouchableHighlight>
+          </View>
+          <Text style={styles.title}>
+            Note
+          </Text>
+          <View style={styles.action}>
+            <TouchableHighlight
+              onPress={this.props.onPost}
+              style={styles.button}>
+              <Text>Post</Text>
+            </TouchableHighlight>
+          </View>
         </View>
+        <View style={styles.textInputContainer}>
+          <TextInput
+            style={styles.textInput}
+            multiline={true}
+            numberOfLines={4}
+            onChangeText={(text) => this.setState({text})}
+            editable={true}
+            maxLength={40}
+            value={this.state.text}
+            placeholder="What you gotta say?"
+          />
        </View>
       </Modal>
     );
@@ -62,9 +81,23 @@ class CreateNoteModal extends Component {
     marginRight: 5,
     marginLeft: 5,
   },
-  actions: {
-    flex: 1,
+  header: {
     flexDirection: 'row',
+  },
+  actions: {
+
+  },
+  textInputContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    borderBottomColor: '#000000',
+    borderBottomWidth: 1,
+  },
+  textInput: {
+    flex: 1,
+    height: 100,
+    borderColor: 'gray',
+    borderWidth: 4,
   },
 });
 
