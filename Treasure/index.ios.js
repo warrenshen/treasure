4dd5d30c9d6a0052d9b6c6193756c7f56712a7a8
warrenshen <warrenzshen@gmail.com>
2016-07-09 12:19:52 -0700

    Render map with react-native-map

--- Treasure/index.ios.js
+++ Treasure/index.ios.js
@@ -11,6 +11,8 @@ import {
   Modal,
 } from 'react-native';
 
+import MapView from 'react-native-maps';
+
 class Treasure extends Component {
 
   // --------------------------------------------------
@@ -50,7 +52,6 @@ class Treasure extends Component {
   render() {
     return (
       <View style={styles.container}>
-
         <Modal
           animationType={"slide"}
           transparent={false}
@@ -84,47 +85,55 @@ class Treasure extends Component {
             style={styles.button}>
           <Text>Send</Text>
         </TouchableHighlight>
-
+        <MapView
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          style={styles.map}
+        />
       </View>
 
     );
   }
 }
 
-  // --------------------------------------------------
-  // Styles
-  // --------------------------------------------------
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#F5FCFF',
-    },
+// --------------------------------------------------
+// Styles
+// --------------------------------------------------
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  button: {
+    backgroundColor: '#eeeeee',
+    padding: 10,
+    marginRight: 5,
+    marginLeft: 5,
+  },
+  actions: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+});
 
-    welcome: {
-      fontSize: 20,
-      textAlign: 'center',
-      margin: 10,
-    },
-
-    instructions: {
-      textAlign: 'center',
-      color: '#333333',
-      marginBottom: 5,
-    },
-
-    button: {
-      backgroundColor: '#eeeeee',
-      padding: 10,
-      marginRight: 5,
-      marginLeft: 5,
-    },
-
-    actions: {
-      flex: 1,
-      flexDirection: 'row',
-    },
-  });
-
-  AppRegistry.registerComponent('Treasure', () => Treasure);
+AppRegistry.registerComponent('Treasure', () => Treasure);
