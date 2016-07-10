acc1ee2e881db86c42c14cc66c266910cd09e304
warrenshen <warrenzshen@gmail.com>
2016-07-10 10:30:38 -0700

    Style info page component

--- Treasure/javascripts/components/InfoPage.js
+++ Treasure/javascripts/components/InfoPage.js
@@ -48,15 +48,18 @@ class InfoPage extends Component {
         renderScene={(route, navigator) => {
           return (
             <View style={styles.container}>
-              <View style={styles.body}>
-                <Text style={styles.bodyText}> {"Place notes at your current location, and see other's notes in the same area. The most popular notes in an area become Treasure! Usually, notes can't be seen unless you're within around 200 meters, but a Treasure is visible from further and further out the more popular it becomes. This app is designed to be hyper-local - so it helps you share your adventures through location rather than time, and so it leads you on an adventure discovering Treasure after Treasure!"}
-                </Text>
-              </View>
-              <View style={styles.createdBy}>
-                <Text style={styles.createdByText}>{"Created by"}</Text>
-              </View>
-              <View style={styles.authors}>
-                <Text style={styles.authorText}>{"Aleks, Kevin, Shimmy, and Warren"}</Text>
+              <View style={styles.section}>
+                <View style={styles.body}>
+                  <Text style={styles.bodyText}>
+                    {"Place notes at your current location, and see other's notes in the same area. The most popular notes in an area become Treasure! Usually, notes can't be seen unless you're within around 200 meters, but a Treasure is visible from further and further out the more popular it becomes. This app is designed to be hyper-local - so it helps you share your adventures through location rather than time, and so it leads you on an adventure discovering Treasure after Treasure!"}
+                  </Text>
+                </View>
+                <View style={styles.createdBy}>
+                  <Text style={styles.createdByText}>{"Created by"}</Text>
+                </View>
+                <View style={styles.authors}>
+                  <Text style={styles.authorText}>{"Aleks, Kevin, Shimmy, and Warren"}</Text>
+                </View>
               </View>
             </View>
           );
@@ -82,8 +85,12 @@ const styles = StyleSheet.create({
     color: '#666666',
     fontSize: 18,
   },
+  createdBy: {
+    paddingBottom: 4,
+  },
   createdByText: {
     color: '#CCCCCC',
+    fontFamily: 'JosefinSansRegularItalic',
     fontSize: 16,
   },
   navbar: {
@@ -100,7 +107,12 @@ const styles = StyleSheet.create({
   },
   authorText: {
     color: '#FF765F',
+    fontFamily: 'JosefinSans',
     fontSize: 18,
+  },
+  section: {
+    flex: 1,
+    padding: 16,
   },
   title: {
     paddingTop: 10,
