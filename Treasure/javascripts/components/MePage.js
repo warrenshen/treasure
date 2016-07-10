a0e5329da7cb72475e3a3324125d2dee1ea1e679
warrenshen <warrenzshen@gmail.com>
2016-07-09 22:42:08 -0700

    Tweak font size and families on create and me pages

--- Treasure/javascripts/components/MePage.js
+++ Treasure/javascripts/components/MePage.js
@@ -23,9 +23,9 @@ class MePage extends Component {
     });
     this.state = {
       dataSource: dataSource.cloneWithRows([
-        '1',
-        '2',
-        '3',
+        'Well hello there cutie pie.',
+        'What\'s good my homie g thangy thang.',
+        'Nothin\' much yo!',
       ]),
       modalIsVisible: false,
     };
@@ -57,8 +57,14 @@ class MePage extends Component {
               <ListView
                 dataSource={this.state.dataSource}
                 renderRow={(rowData) => (
-                  <View style={styles.row}>
-                    <Text>{rowData}</Text>
+                  <View style={styles.item}>
+                    <View style={styles.row}>
+                      <Text style={styles.text}>{rowData}</Text>
+                    </View>
+                    <View style={styles.subrow}>
+                      <Text style={styles.subtext}>{'Berkeley, California'}</Text>
+                      <Text style={styles.votes}>{'33'}</Text>
+                    </View>
                   </View>
                 )}
                 style={styles.list}
@@ -80,7 +86,6 @@ const styles = StyleSheet.create({
     paddingTop: 64,
   },
   list: {
-    flexDirection: 'column',
     flex: 1,
   },
   navbar: {
@@ -95,15 +100,39 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 2,
   },
-  row: {
-    borderColor: 'gray',
+  item: {
+    padding: 12,
+    borderColor: '#CCCCCC',
     borderBottomWidth: 1,
+  },
+  row: {
+
+  },
+  subrow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingTop: 12,
+  },
+  subtext: {
+    flex: 1,
+    color: '#CCCCCC',
+    fontFamily: 'JosefinSans-BoldItalic',
+    fontSize: 16,
+  },
+  text: {
+    color: '#333333',
+    fontSize: 18,
   },
   title: {
     paddingTop: 10,
     color: 'white',
     fontFamily: 'JosefinSans-Bold',
     fontSize: 24,
+  },
+  votes: {
+    color: '#666666',
+    fontFamily: 'JosefinSans-Bold',
+    fontSize: 18,
   },
 });
 
